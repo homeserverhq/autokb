de59a5f409a2a4afadcd6baa8041d204adea8e95
Expert <dev@example.com>
2026-08-19 05:28:29 -0500

    Updates and bugfixes

--- src/plugins/eBiblePlugin.py
+++ src/plugins/eBiblePlugin.py
@@ -129,7 +129,7 @@ class eBiblePlugin(BaseSubscription):
     metadata = {
         "name": "eBiblePlugin",
         "display_name": "Bible Scriptures",
-        "description": "Downloads all Bible scripture for the selected version and applies custom chunking strategy on each chapter. Set cron to yearly (0 0 1 1 *) since Bible data doesn't change (Hebrews 13:8). All data is courtesy of  https://bible.helloao.org/ and https://ebible.org/",
+        "description": "Downloads all Bible scripture for the selected version and applies custom chunking strategy on each chapter. Set cron to leap-day (0 0 30 2 *) since Bible data doesn't change (Hebrews 13:8). All data is courtesy of  https://bible.helloao.org/ and https://ebible.org/",
         "sub_type": "SCHEDULED",
     }
 
